81a2347742125bb24d63049695135da1bb0de960
Oli Jones <oliver.jones@diamond.ac.uk>
2026-06-12 08:21:33 +0000

    Add query methods to SpecTel class

--- src/fastcsflame/spectrometer_telecommunicator.py
+++ src/fastcsflame/spectrometer_telecommunicator.py
@@ -4,6 +4,7 @@ from socket import socket
 class SpectrometerTelecommunicator:
     ip: str
     port: int
+    recieve_buffer_size: int = 1024
     socket_obj: socket | None = None
 
     def __init__(self, ip: str, port: int):
@@ -17,3 +18,31 @@ class SpectrometerTelecommunicator:
         self.socket_obj = socket()
         self.socket_obj.bind((self.ip, self.port))
         # TODO: listen for initial message from device and validate??
+
+    def _small_query(self, query: str) -> str:
+        if self.socket_obj is None:
+            # TODO: add handling when socket is None
+            return ""
+
+        self.socket_obj.send(query.encode("ascii"))
+
+        raw_response = self.socket_obj.recv(self.recieve_buffer_size)
+        # TODO: look for acknowledgement characters here
+        response = raw_response.decode("ascii")
+
+        return response
+
+    def _big_query(self, query: str, end_string="65533") -> str:
+        if self.socket_obj is None:
+            # TODO: add handling when socket is None
+            return ""
+
+        self.socket_obj.send(query.encode("ascii"))
+
+        response = ""
+        while end_string not in response:
+            raw_response = self.socket_obj.recv(self.recieve_buffer_size)
+            response.join(raw_response.decode("ascii"))
+        # TODO: Add a failsafe incase transmission is stopped half way through
+        # TODO: look for acknowledgement characters here
+        return response
