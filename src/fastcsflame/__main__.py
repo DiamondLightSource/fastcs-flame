fa95e3a8d2660ae814e0b5bea10303db86f6f264
Oli Jones <oliver.jones@diamond.ac.uk>
2026-06-12 13:53:44 +0000

    Launch fastcs IOC from main

--- src/fastcsflame/__main__.py
+++ src/fastcsflame/__main__.py
@@ -3,6 +3,14 @@
 from argparse import ArgumentParser
 from collections.abc import Sequence
 
+from fastcs.launch import FastCS
+from fastcs.transports.epics.ca import EpicsCATransport
+
+from fastcsflame.flame_controller import FlameController
+from fastcsflame.spectrometer_telecommunicator import (
+    SpectrometerTelecommunicator as SpecTel,
+)
+
 from . import __version__
 
 __all__ = ["main"]
@@ -18,6 +26,17 @@ def main(args: Sequence[str] | None = None) -> None:
         version=__version__,
     )
     parser.parse_args(args)
+
+    spec_tel_obj = SpecTel("172.23.91.5", 7016)
+
+    epics_ca = EpicsCATransport()
+
+    flame_controller = FlameController(spec_tel_obj)
+    flame_controller.set_path(["FLAME"])
+
+    fastcs = FastCS(flame_controller, [epics_ca])
+
+    fastcs.run()
 
 
 if __name__ == "__main__":
