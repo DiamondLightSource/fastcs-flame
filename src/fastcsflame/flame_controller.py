ad7554cb234f2e6e6c87d3ec6a146028ccf3b07b
Oli Jones <oliver.jones@diamond.ac.uk>
2026-06-15 11:42:01 +0000

    Change scan trigger attribute to command

--- src/fastcsflame/flame_controller.py
+++ src/fastcsflame/flame_controller.py
@@ -3,7 +3,8 @@ from dataclasses import dataclass
 import numpy as np
 from fastcs.attributes import AttributeIO, AttributeIORef, AttrR, AttrRW, AttrW
 from fastcs.controllers import Controller
-from fastcs.datatypes import Bool, Int, Waveform
+from fastcs.datatypes import Int, Waveform
+from fastcs.methods.command import command
 from fastcs.util import ONCE
 
 from fastcsflame.spectrometer_telecommunicator import (
@@ -53,34 +54,6 @@ class ScanDataIO(AttributeIO[np.ndarray, ScanDataIORef]):
         await attr.update(scan_data)
 
 
-class ScanTriggerIORef(AttributeIORef):
-    def __init__(self):
-        super().__init__(update_period=None)
-
-
-class ScanTriggerIO(AttributeIO[bool, ScanTriggerIORef]):
-    spec_tel_obj: SpecTel
-
-    def __init__(
-        self,
-        scan_data_attribte: AttrR[np.ndarray, ScanDataIORef],
-        spec_tel_obj: SpecTel,
-    ):
-        self.spec_tel_obj = spec_tel_obj
-        self.scan_data_attribute = scan_data_attribte
-
-    async def send(self, attr: AttrW[bool, ScanTriggerIORef], value: bool):
-        if not value:
-            # Input false?
-            # Why even bother?
-            return
-
-        new_scan_data = self.spec_tel_obj.scan()
-        await self.scan_data_attribute.update(new_scan_data)
-        # Set the value back to False here??
-        # How do I do this in FastCS? I see no method
-
-
 class FlameController(Controller):
     spec_tel_obj: SpecTel
 
@@ -90,9 +63,6 @@ class FlameController(Controller):
     scan_data: AttrR[np.ndarray, ScanDataIORef] = AttrR(
         Waveform(int, shape=(2048,)), io_ref=ScanDataIORef()
     )
-    trigger_scan: AttrW[bool, ScanTriggerIORef] = AttrW(
-        Bool(), io_ref=ScanTriggerIORef()
-    )
 
     def __init__(self, ip: str, port: int):
         self.spec_tel_obj = SpecTel(ip, port)
@@ -101,10 +71,14 @@ class FlameController(Controller):
             ios=[
                 IntegrationTimeIO(self.spec_tel_obj),
                 ScanDataIO(self.spec_tel_obj),
-                ScanTriggerIO(self.scan_data, self.spec_tel_obj),
             ]
         )
 
     async def connect(self):
         await super().connect()
         self.spec_tel_obj.connect()
+
+    @command()
+    async def scan(self):
+        new_scan_data = self.spec_tel_obj.scan()
+        await self.scan_data.update(new_scan_data)
