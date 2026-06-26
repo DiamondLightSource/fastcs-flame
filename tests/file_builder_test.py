9da4e92a701ba33da29d7d4f114c6889a8161f19
Oli Jones <oliver.jones@diamond.ac.uk>
2026-06-26 10:44:14 +0000

    Write structure tests for file builder

--- tests/file_builder_test.py
+++ tests/file_builder_test.py
@@ -1,3 +1,4 @@
+import h5py
 import numpy as np
 import pytest
 
@@ -17,20 +18,73 @@ def basic_file():
 
 
 def test_entry_structure(basic_file):
-    pass
+    entry_group = basic_file["ENTRY"]
+    assert isinstance(entry_group, h5py.Group)
+
+    title_field = entry_group["title"]
+    assert isinstance(title_field, h5py.Dataset)
+    start_time_field = entry_group["start_time"]
+    assert isinstance(start_time_field, h5py.Dataset)
+    end_time_field = entry_group["end_time"]
+    assert isinstance(end_time_field, h5py.Dataset)
+    experiment_type_field = entry_group["experiment_type"]
+    assert isinstance(experiment_type_field, h5py.Dataset)
 
 
 def test_definition_structure(basic_file):
-    pass
+    entry_group = basic_file["ENTRY"]
+
+    definition_field = entry_group["definition"]
+    assert isinstance(definition_field, h5py.Dataset)
+    assert "@version" in definition_field.attrs
+    assert "@URL" in definition_field.attrs
 
 
 def test_instrument_structure(basic_file):
-    pass
+    entry_group = basic_file["ENTRY"]
+
+    instrument_group = entry_group["INSTRUMENT"]
+    assert isinstance(instrument_group, h5py.Group)
+
+    beam_type_group = entry_group["beam_TPYE"]
+    assert isinstance(beam_type_group, h5py.Group)
+    detector_type_group = entry_group["detector_TYPE"]
+    assert isinstance(detector_type_group, h5py.Group)
+
+    parameter_reliability_field = beam_type_group["parameter_reliability"]
+    assert isinstance(parameter_reliability_field, h5py.Dataset)
+    detector_channel_type_field = entry_group["detector_channel_type"]
+    assert isinstance(detector_channel_type_field, h5py.Dataset)
 
 
 def test_sample_structure(basic_file):
-    pass
+    entry_group = basic_file["ENTRY"]
+
+    sample_group = entry_group["SAMPLE"]
+    assert isinstance(sample_group, h5py.Group)
+
+    name_field = sample_group["name"]
+    assert isinstance(name_field, h5py.Dataset)
+    sample_id_field = sample_group["sample_id_field"]
+    assert isinstance(sample_id_field, h5py.Dataset)
 
 
 def test_data_structure(basic_file):
-    pass
+    entry_group = basic_file["ENTRY"]
+
+    data_group = entry_group["DATA"]
+    assert isinstance(data_group, h5py.Group)
+
+    assert "axes" in data_group.attrs
+    assert "signal" in data_group.attrs
+
+    dataset = data_group["DATA"]
+    assert isinstance(dataset, h5py.Dataset)
+
+    time_axis = data_group["TIME_AXIS"]
+    assert isinstance(time_axis, h5py.Dataset)
+    assert "long_name" in time_axis.attrs
+
+    wavelength_axis = data_group["WAVELENGTH_AXIS"]
+    assert isinstance(wavelength_axis, h5py.Dataset)
+    assert "long_name" in wavelength_axis.attrs
