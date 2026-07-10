9fd76c0198b360598d02ee7ac22d9a60b246ae87
Oli Jones <oliver.jones@diamond.ac.uk>
2026-07-10 14:50:25 +0000

    Added filebuilder fixture

--- tests/file_builder_test.py
+++ tests/file_builder_test.py
@@ -1,34 +1,101 @@
+from dataclasses import dataclass
+from pathlib import Path
+
 import h5py
 import numpy as np
+from numpy.typing import NDArray
+from pytest import fixture
 
 from fastcsflame.file_builder import FileBuilder
 
 
-def test_creation(tmp_path):
-    wavelengths = np.array(range(2044), dtype=np.float64)
+@dataclass
+class FileParameters:
+    file_path: str
+    file_name: str
+    wavelength_array: NDArray[np.float64]
+    data: NDArray[np.int64]
 
-    fb = FileBuilder(wavelengths)
-    fb.create_file(tmp_path, "data")
-    data = np.zeros(wavelengths.shape, dtype=np.int64)
-    fb.add_scan(data)
+
+BASIC_DUMMY_PATH = "/data"
+BASIC_DUMMY_NAME = "data"
+EXTENDED_DUMMY_PATH = "/dls/science/more"
+ALTERNATE_DUMMY_NAME = "dt"
+BASIC_WAVLENGTH_ARRAY = np.array(range(2044), dtype=np.float64)
+ALTERNATE_WAVELENGTH_ARRAY = np.array(range(1000), dtype=np.float64)
+BASIC_DATA_ARRAY = np.array([range(2044)])
+ALTERNATE_DATA_ARRAY = np.array([range(1000)])
+LARGER_DATA_ARRAY = np.array([[i + j for j in range(2044)] for i in range(10)])
+EMPTY_DATA_ARRAY = np.array([])
+
+
+@fixture(
+    params=[
+        # Basic file
+        FileParameters(
+            BASIC_DUMMY_PATH,
+            BASIC_DUMMY_NAME,
+            BASIC_WAVLENGTH_ARRAY,
+            BASIC_DATA_ARRAY,
+        ),
+        # Test extended file path and different name
+        FileParameters(
+            EXTENDED_DUMMY_PATH,
+            ALTERNATE_DUMMY_NAME,
+            BASIC_WAVLENGTH_ARRAY,
+            BASIC_DATA_ARRAY,
+        ),
+        # Test different wavelengths
+        FileParameters(
+            BASIC_DUMMY_PATH,
+            BASIC_DUMMY_NAME,
+            ALTERNATE_WAVELENGTH_ARRAY,
+            ALTERNATE_DATA_ARRAY,
+        ),
+        # Test more rows of data
+        FileParameters(
+            BASIC_DUMMY_PATH,
+            BASIC_DUMMY_NAME,
+            BASIC_WAVLENGTH_ARRAY,
+            LARGER_DATA_ARRAY,
+        ),
+        # Test no rows of data
+        FileParameters(
+            BASIC_DUMMY_PATH,
+            BASIC_DUMMY_NAME,
+            BASIC_WAVLENGTH_ARRAY,
+            EMPTY_DATA_ARRAY,
+        ),
+    ]
+)
+def h5file(tmp_path, request):
+    params = request.param
+
+    params.file_path = str(tmp_path) + params.file_path
+
+    Path(params.file_path).mkdir(parents=True, exist_ok=True)
+
+    fb = FileBuilder(params.wavelength_array)
+    fb.create_file(params.file_path, params.file_name)
+
+    for data_row in params.data:
+        fb.add_scan(data_row)
     fb.close_file()
+
+    return params
+
+
+def test_creation(h5file):
+    params = h5file
 
     # Test the file is created in the correct place
-    file = h5py.File(f"{tmp_path}/data.h5", "r")
+    file = h5py.File(f"{params.file_path}/{params.file_name}.h5", "r")
     assert isinstance(file, h5py.File)
 
 
-def test_structure(tmp_path):
-    wavelengths = np.array(range(2044))
-
-    fb = FileBuilder(wavelengths)
-    fb.create_file(tmp_path, "data")
-    data = np.zeros_like(wavelengths)
-    fb.add_scan(data)
-    fb.close_file()
-
-    file = h5py.File(f"{tmp_path}/data.h5", "r")
-    assert isinstance(file, h5py.File)
+def test_structure(h5file):
+    params = h5file
+    file = h5py.File(f"{params.file_path}/{params.file_name}.h5", "r")
 
     entry_group = file["entry"]
     assert isinstance(entry_group, h5py.Group)
@@ -47,29 +114,17 @@ def test_structure(tmp_path):
     assert "long_name" in wavelength_axis.attrs
 
 
-def test_values(tmp_path):
-    wavelengths = np.array(range(2044))
-
-    fb = FileBuilder(wavelengths)
-    fb.create_file(tmp_path, "data")
-    datas = []
-    iterations = 5
-    for _ in range(iterations):
-        data = np.random.randint(900, high=1100, size=wavelengths.shape)
-        fb.add_scan(data)
-        datas.append(data)
-    fb.close_file()
-
-    # Test the file is created in the correct place
-    file = h5py.File(f"{tmp_path}/data.h5", "r")
-    assert isinstance(file, h5py.File)
+def test_values(h5file):
+    params = h5file
+    file = h5py.File(f"{params.file_path}/{params.file_name}.h5", "r")
 
     dataset = file["entry/data/data"]
     assert isinstance(dataset, h5py.Dataset)
 
-    assert np.array_equal(np.array(dataset), np.array(datas))
+    if len(params.data) != 0:
+        assert np.array_equal(np.array(dataset), params.data)
 
     wavlength_axis = file["entry/data/wavelength_axis"]
     assert isinstance(wavlength_axis, h5py.Dataset)
 
-    assert np.array_equal(np.array(wavlength_axis), np.array(wavelengths))
+    assert np.array_equal(np.array(wavlength_axis), params.wavelength_array)
