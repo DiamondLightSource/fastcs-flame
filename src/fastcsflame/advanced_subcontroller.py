afba72ed5aced23643bacb961e5d53eff20b70e1
Oli Jones <oliver.jones@diamond.ac.uk>
2026-08-05 12:58:31 +0000

    Add PV for lamp status

--- src/fastcsflame/advanced_subcontroller.py
+++ src/fastcsflame/advanced_subcontroller.py
@@ -1,8 +1,10 @@
 from fastcs.attributes import AttrRW
 from fastcs.controllers import Controller
-from fastcs.datatypes import Int
+from fastcs.datatypes import Bool, Int
 
 from fastcsflame.flame_controller_attributes import (
+    DummyBoolIO,
+    DummyBoolIORef,
     IntegrationTimeIO,
     IntegrationTimeIORef,
 )
@@ -10,15 +12,17 @@ from fastcsflame.flame_controller_attributes import (
 
 class FlameController(Controller):
     integration_time: AttrRW[int, IntegrationTimeIORef]
+    lamp: AttrRW[bool, DummyBoolIORef]
 
     def __init__(self, spec_tel_obj):
-        super().__init__(
-            ios=[
-                IntegrationTimeIO(),
-            ]
-        )
+        super().__init__(ios=[IntegrationTimeIO(), DummyBoolIO()])
 
         self.spec_tel_obj = spec_tel_obj
         self.integration_time = AttrRW(
             Int(), io_ref=IntegrationTimeIORef(self.spec_tel_obj)
         )
+        self.lamp = AttrRW(Bool(), io_ref=DummyBoolIORef(True))
+        self.lamp.add_on_update_callback(self.lamp_change)
+
+    async def lamp_change(self, new_lamp_status):
+        pass
