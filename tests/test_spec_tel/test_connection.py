2f6cc06af79585b195c5794dd9ab3fb1281760ee
Oli Jones <oliver.jones@diamond.ac.uk>
2026-08-13 13:09:13 +0000

    Add connection tests

--- tests/test_spec_tel/test_connection.py
+++ tests/test_spec_tel/test_connection.py
@@ -1,10 +1,12 @@
 import asyncio
+from unittest.mock import patch
 
 import pytest
 from test_common import (
     DEFAULT_IP,
     DEFAULT_PORT,
     run_dummy_spectrometer,
+    start_connection,
     start_spec_tel_object,
 )
 
@@ -12,6 +14,9 @@ from dummy_spectrometer import DummySpectrometer
 from fastcsflame.spectrometer_telecommunicator import (
     SpectrometerTelecommunicator as SpecTel,
 )
+from fastcsflame.spectrometer_telecommunicator import UnexpectedResponseError
+
+DEFAULT_SLEEP_TIME = 2
 
 
 @pytest.mark.asyncio
@@ -25,3 +30,45 @@ async def test_connection():
 
     async with start_spec_tel_object(spec_tel_obj=spec_tel_obj):
         assert spec_tel_obj.connected
+
+
+@pytest.mark.asyncio
+async def test_bad_socket():
+    spec_tel_obj = SpecTel(DEFAULT_IP, DEFAULT_PORT)
+    with pytest.raises((ConnectionRefusedError, ConnectionResetError)):
+        async with start_spec_tel_object(spec_tel_obj=spec_tel_obj):
+            pass
+
+
+@pytest.mark.asyncio
+async def test_bad_initial_connection_message():
+
+    with patch("dummy_spectrometer.TELNET_STARTUP_MESSAGE", b"\x15"):
+        with pytest.raises(UnexpectedResponseError):
+            async with start_connection() as (_, _):
+                pass
+
+
+@pytest.mark.asyncio
+async def test_device_connection_lost():
+    async with start_connection() as (dummy_spec_obj, spec_tel_obj):
+        await asyncio.sleep(DEFAULT_SLEEP_TIME)
+        await dummy_spec_obj.disconnect()
+        assert not spec_tel_obj.connected
+
+
+@pytest.mark.asyncio
+async def test_reconnection():
+    async with start_connection() as (dummy_spec_obj, spec_tel_obj):
+        await asyncio.sleep(DEFAULT_SLEEP_TIME)
+        await dummy_spec_obj.disconnect()
+        await asyncio.sleep(1.0)
+        assert not spec_tel_obj.connected
+
+        await asyncio.sleep(DEFAULT_SLEEP_TIME)
+        asyncio.create_task(run_dummy_spectrometer(dummy_spec_obj))
+        await dummy_spec_obj.waiting_for_connection.wait()
+        await spec_tel_obj.restart_connection()
+        assert spec_tel_obj.connected
+
+        await asyncio.sleep(DEFAULT_SLEEP_TIME)
