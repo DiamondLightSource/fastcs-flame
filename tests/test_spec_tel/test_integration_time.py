c1121f745861f75b501e7b15062e6677682fd022
Oli Jones <oliver.jones@diamond.ac.uk>
2026-08-14 09:54:25 +0000

    Add tests for setting integration time

--- tests/test_spec_tel/test_integration_time.py
+++ tests/test_spec_tel/test_integration_time.py
@@ -10,10 +10,13 @@ DEFAULT_INTEGRATION_TIME = 10
 CHANGED_INTEGRATION_TIME = 8
 
 INTEGRATION_TIME_GET = "?I"
-INTEGRATION_TIME_SET = "I10\n"
+INTEGRATION_TIME_SET = f"I{CHANGED_INTEGRATION_TIME}\n"
 
 DEFAULT_INTEGRATION_TIME_GET_RESPONSE = DummySpectrometer.wrap_response(
     INTEGRATION_TIME_GET, str(DEFAULT_INTEGRATION_TIME)
+)
+DEFAULT_INTEGRATION_TIME_SET_RESPONSE = DummySpectrometer.wrap_response(
+    INTEGRATION_TIME_SET, ""
 )
 
 
@@ -58,19 +61,27 @@ async def test_get_integration_time_bad_response():
 
 @pytest.mark.asyncio
 async def test_set_integration_time_message():
-    pass
-
-
-@pytest.mark.asyncio
-async def test_set_integration_time_decoding():
-    pass
+    async with start_mock_socket() as (spec_tel_obj, socket_obj, event_loop):
+        # Simulate expected response so no errors are thrown
+        # Easier than mocking all the various methods involved
+        event_loop.sock_recv = AsyncMock(
+            return_value=DEFAULT_INTEGRATION_TIME_SET_RESPONSE
+        )
+        # This also tests the decoding as an error would be raised here if its wrong
+        await spec_tel_obj.set_integration_time(CHANGED_INTEGRATION_TIME)
+        socket_obj.send.assert_called_once_with(INTEGRATION_TIME_SET.encode())
 
 
 @pytest.mark.asyncio
 async def test_set_integration_time():
-    pass
+    async with start_connection() as (dummy_spec_obj, spec_tel_obj):
+        await spec_tel_obj.set_integration_time(CHANGED_INTEGRATION_TIME)
+        assert dummy_spec_obj.integration_time == CHANGED_INTEGRATION_TIME
 
 
 @pytest.mark.asyncio
-async def test_set_integration_time_bad_response():
-    pass
+async def test_set_integration_time_bad_response(loguru_caplog):
+    async with start_connection() as (dummy_spec_obj, spec_tel_obj):
+        dummy_spec_obj.handle_set_integration_time_request = lambda request: ""
+        await spec_tel_obj.set_integration_time(CHANGED_INTEGRATION_TIME)
+        assert "Negative acknowledgement in response" in loguru_caplog.text
