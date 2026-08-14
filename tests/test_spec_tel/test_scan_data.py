34e2276ce100436de2a7da045c77549226719ff5
Oli Jones <oliver.jones@diamond.ac.uk>
2026-08-14 12:53:48 +0000

    Add tests for scan data

---
+++ tests/test_spec_tel/test_scan_data.py
@@ -0,0 +1,118 @@
+from unittest.mock import AsyncMock
+
+import pytest
+from test_common import start_connection, start_mock_socket
+
+from common import lists_equal
+from dummy_spectrometer import DummySpectrometer
+from fastcsflame.spectrometer_telecommunicator import UnexpectedResponseError
+
+LAST_SCAN_DATA = list(range(100))
+CHANGED_SCAN_DATA = list(range(1, 101))
+
+GET_LAST_SCAN_DATA = "Z"
+SCAN = "S"
+
+GET_LAST_SCAN_RESPONSE = DummySpectrometer.wrap_response(
+    GET_LAST_SCAN_DATA,
+    "65535 0 1 8 0 0 0 " + " ".join([str(x) for x in LAST_SCAN_DATA]) + " 65533 ",
+)
+SCAN_RESPONSE = DummySpectrometer.wrap_response(
+    SCAN,
+    "65535 0 1 8 0 0 0 " + " ".join([str(x) for x in CHANGED_SCAN_DATA]) + " 65533 ",
+    delimeter=b"\x02",
+)
+
+
+@pytest.mark.asyncio
+async def test_get_last_scan_data_message():
+    async with start_mock_socket() as (spec_tel_obj, socket_obj, event_loop):
+        event_loop.sock_recv = AsyncMock(return_value=GET_LAST_SCAN_RESPONSE)
+        await spec_tel_obj.get_last_scan()
+        socket_obj.send.assert_called_once_with(GET_LAST_SCAN_DATA.encode())
+
+
+@pytest.mark.asyncio
+async def test_get_last_scan_data_decoding():
+    async with start_mock_socket() as (spec_tel_obj, socket_obj, event_loop):
+        event_loop.sock_recv = AsyncMock(return_value=GET_LAST_SCAN_RESPONSE)
+        get_last_scan_response = await spec_tel_obj.get_last_scan()
+        assert lists_equal(get_last_scan_response, LAST_SCAN_DATA)
+
+
+@pytest.mark.asyncio
+async def test_get_last_scan_data():
+    async with start_connection() as (dummy_spec_obj, spec_tel_obj):
+        recieved_last_scan = await spec_tel_obj.get_last_scan()
+        assert lists_equal(dummy_spec_obj.last_scan_data, recieved_last_scan)
+
+
+@pytest.mark.asyncio
+async def test_get_last_scan_data_short_response():
+    async with start_connection() as (dummy_spec_obj, spec_tel_obj):
+
+        async def empty_return_awaitable():
+            return "one thousand"
+
+        dummy_spec_obj.handle_get_last_scan_request = empty_return_awaitable
+        with pytest.raises(UnexpectedResponseError):
+            await spec_tel_obj.get_last_scan()
+
+
+@pytest.mark.asyncio
+async def test_get_last_scan_data_bad_response():
+    async with start_connection() as (dummy_spec_obj, spec_tel_obj):
+
+        async def empty_return_awaitable():
+            return "one hundred and twenty two thousand two hundred and thirty three"
+
+        dummy_spec_obj.handle_get_last_scan_request = empty_return_awaitable
+        with pytest.raises(UnexpectedResponseError):
+            await spec_tel_obj.get_last_scan()
+
+
+@pytest.mark.asyncio
+async def test_scan_message():
+    async with start_mock_socket() as (spec_tel_obj, socket_obj, event_loop):
+        event_loop.sock_recv = AsyncMock(return_value=SCAN_RESPONSE)
+        await spec_tel_obj.scan()
+        socket_obj.send.assert_called_once_with(SCAN.encode())
+
+
+@pytest.mark.asyncio
+async def test_scan_decoding():
+    async with start_mock_socket() as (spec_tel_obj, socket_obj, event_loop):
+        event_loop.sock_recv = AsyncMock(return_value=SCAN_RESPONSE)
+        scan_data_recieved = await spec_tel_obj.scan()
+        assert lists_equal(scan_data_recieved, CHANGED_SCAN_DATA)
+
+
+@pytest.mark.asyncio
+async def test_data():
+    async with start_connection() as (dummy_spec_obj, spec_tel_obj):
+        recieved_last_scan = await spec_tel_obj.scan()
+        assert lists_equal(dummy_spec_obj.last_scan_data, recieved_last_scan)
+
+
+@pytest.mark.asyncio
+async def test_scan_data_short_response():
+    async with start_connection() as (dummy_spec_obj, spec_tel_obj):
+
+        async def empty_return_awaitable():
+            return "one thousand"
+
+        dummy_spec_obj.handle_scan_request = empty_return_awaitable
+        with pytest.raises(UnexpectedResponseError):
+            await spec_tel_obj.scan()
+
+
+@pytest.mark.asyncio
+async def test_scan_data_bad_response():
+    async with start_connection() as (dummy_spec_obj, spec_tel_obj):
+
+        async def empty_return_awaitable():
+            return "one hundred and twenty two thousand two hundred and thirty three"
+
+        dummy_spec_obj.handle_scan_request = empty_return_awaitable
+        with pytest.raises(UnexpectedResponseError):
+            await spec_tel_obj.scan()
