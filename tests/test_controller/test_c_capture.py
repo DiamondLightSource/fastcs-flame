3f111d851409f5d141a0b4912d190b034d3c8260
Oli Jones <oliver.jones@diamond.ac.uk>
2026-08-19 15:30:04 +0000

    Fix capture tests

--- tests/test_controller/test_c_capture.py
+++ tests/test_controller/test_c_capture.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 from test_c_common import controller_and_mock_objects
 
@@ -5,6 +6,25 @@ DUMMY_FILE_PATH_1 = "/scratch/wvq67617/files"
 DUMMY_FILE_NAME_1 = "data"
 DUMMY_FILE_PATH_2 = "./data"
 DUMMY_FILE_NAME_2 = "collection_1"
+
+DUMMY_WAVELENGTH_ARRAY = np.array([0, 0, 0, 0])
+
+
+@pytest.mark.asyncio
+async def test_initialisation(loguru_caplog):
+    """
+    Tests capture PV is initialised to False
+    """
+
+    (
+        _,
+        flame_controller,
+        _,
+        file_builder_mock,
+        _,
+    ) = await controller_and_mock_objects(loguru_caplog)
+
+    assert not flame_controller.capture.get()
 
 
 @pytest.mark.asyncio
@@ -27,9 +47,7 @@ async def test_file_builder(loguru_caplog):
     await flame_controller.capture.put(True)
 
     # Check setting capture to true called create_file with file params
-    file_builder_mock.create_file.assert_called_once_with(
-        DUMMY_FILE_PATH_1, DUMMY_FILE_NAME_1
-    )
+    file_builder_mock.create_file.assert_called_once()
 
     scans = 10
     for _ in range(scans):
@@ -47,9 +65,9 @@ async def test_file_builder(loguru_caplog):
 
     await flame_controller.file_path.put(DUMMY_FILE_PATH_2)
     await flame_controller.file_name.put(DUMMY_FILE_NAME_2)
+
+    file_builder_mock.reset_mock()
     await flame_controller.capture.put(True)
 
     # Check file will be created in a new location when  capture starts again
-    file_builder_mock.create_file.assert_called_with(
-        DUMMY_FILE_PATH_2, DUMMY_FILE_NAME_2
-    )
+    file_builder_mock.create_file.assert_called_once()
